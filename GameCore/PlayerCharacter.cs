7318354660396011a5cafd8a604a5b65324241ed
Myo_Thet <mr.myothet@gmail.com>
2020-02-21 11:11:30 +0630

    implement some features in production code

--- GameCore/PlayerCharacter.cs
+++ GameCore/PlayerCharacter.cs
@@ -1,9 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GameCore
 {
     public class PlayerCharacter
     {
+        public int Health { get; private set; } = 100;
+        public bool IsDead { get; private set; }
+        public int DamageResistance { get; set; }
+        public string Race { get; set; }
+        public CharacterClass CharacterClass { get; set; }
+        public DateTime LastSleepTime { get; set; }
+
+        public List<MagicalItem> MagicalItems { get; set; } = new List<MagicalItem>();
+        public int MagicalPower
+        {
+            get
+            {
+                return MagicalItems.Sum(x => x.Power);
+            }
+        }
+
+        public List<Weapon> Weapons { get; set; } = new List<Weapon>();
+        public int WeaponsValue
+        {
+            get
+            {
+                return Weapons.Sum(x => x.Value);
+            }
+        }
+
         public void Hit(int damage)
         {
             int raceSpecificDamageResistance = 0;
@@ -23,9 +50,39 @@ namespace GameCore
             }
         }
 
-        public int Health { get; private set; } = 100;
-        public bool IsDead { get; private set; }
-        public int DamageResistance { get; set; }
-        public string Race { get; set; }
+        public void CastHealingSpell()
+        {
+            if (CharacterClass == CharacterClass.Healer)
+            {
+                Health = 100;
+            }
+            else
+            {
+                Health += 10;
+            }
+        }
+
+        public void ReadHealthScroll()
+        {
+            var daysSinceLastSleep = DateTime.Now.Subtract(LastSleepTime).Days;
+
+            if (daysSinceLastSleep <= 2)
+            {
+                Health = 100;
+            }
+        }
+
+        public void UseMagicalItem(string itemName)
+        {
+            int powerReduction = 10;
+
+            if (Race == "Elf")
+            {
+                powerReduction = 0;
+            }
+
+            var itemToReduce = MagicalItems.First(item => item.Name == itemName);
+            itemToReduce.Power -= powerReduction;
+        }
     }
 }
